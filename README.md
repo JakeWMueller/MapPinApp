eddd28ca690f657dd0acbd33ae0167d0ce599864
JakeWMueller <jake.william.mueller@gmail.com>
2021-09-10 10:31:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MapPin-App
+A Guestbook type application for giving reviews and descriptions of any location in the world. Built using Node.js, React, MongoDB, Axios, and MapBox.
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,53 @@
+import { Cancel, Room } from "@material-ui/icons";
+import axios from "axios";
+import { useRef, useState } from "react";
+import "./login.css";
+
+//Login button and functionality using local storage
+export default function Login({ setShowLogin, setCurrentUsername, myStorage }) {
+    const [error, setError] = useState(false);
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const user = {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value,
+        };
+        try {
+            // Utilizing axios for posting to our database since async needs a promise
+            const res = await axios.post("/users/login", user);
+            setCurrentUsername(res.data.username);
+            myStorage.setItem('user', res.data.username);
+            setShowLogin(false)
+        } catch (err) {
+            setError(true);
+        }
+    };
+
+    return (
+        <div className="loginContainer">
+            <div className="logo">
+                <span>
+                    <div>
+                    <h4 className="mapWord">Map</h4> <Room className="logoIcon"/><h4 className="appWord"> App</h4>
+                    </div>
+                </span>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input autoFocus placeholder="username" ref={usernameRef} />
+                <input
+                    type="password"
+                    min="6"
+                    placeholder="password"
+                    ref={passwordRef}
+                />
+                <button className="loginBtn" type="submit">
+                    Log In
+                </button>
+                {error && <span className="failure">Something went wrong </span>}
+            </form>
+            <Cancel className="loginCancel" onClick={() => setShowLogin(false)} />
+        </div> 
+    );
+}
