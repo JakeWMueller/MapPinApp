8018cc3cfb1b7ec2e67d645dab92eba612b410c3
JakeWMueller <jake.william.mueller@gmail.com>
2021-09-10 15:03:17 -0500

    app.use and app.get functionality in index.js

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ mongoose
     app.use("/api/pins", pinRoute);
 
 const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(PORT, ()=>{
     console.log("Backend server is running on port ${ PORT }.");
 });
